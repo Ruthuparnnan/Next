42534d92fae94e52c566815379394e4524fe41a7
Ruthuparnnan <ruthuparnnan123@gmail.com>
2025-01-18 13:36:55 +0530

    next icon and tailwind recap

--- src/app/auth/login/page.js
+++ src/app/auth/login/page.js
@@ -1,3 +1,10 @@
 export default function Page() {
-  return <h1>Please Login to the application or go to hell</h1>;
+  return (
+    <div className="flex gap-5 mt-3 ml-3 items-center">
+      <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black" />
+      <h1 className="font-bold text-md">
+        Please Login to the application or go to hell
+      </h1>
+    </div>
+  );
 }
